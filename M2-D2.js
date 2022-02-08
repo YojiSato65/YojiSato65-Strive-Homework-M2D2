9565750061681ffdce97ab9239c99123e6f996b8
YojiSato65 <yojisato65@gmail.com>
2022-02-08 17:35:16 +0200

    Solved ex.3

---
+++ M2-D2.js
@@ -0,0 +1,33 @@
+
+// MANDATORY JS EXERCISES (create a different file for this exercise)//
+//     1) Save this array in a variable called genres: ["Comedy", "Drama", "Romance", "Horror", "Sci-Fi", "Documentary"]//
+let genres = [
+    "Comedy", 
+    "Drama", 
+    "Romance", 
+    "Horror", 
+    "Sci-Fi", 
+    "Documentary"
+]
+console.log(genres)
+
+//     2) Create an unordered list using JavaScript and save it in a variable//
+let newUl = document.createElement('ul')
+
+//     3) Cycle the array and create a list-item via JavaScript for every element//
+const cycleArray = function () {
+    for (let i = 0; i < genres.length; i ++) {
+        let newLiNode = genres[i]
+        newLiNode = document.createElement('li')
+    }
+
+}
+cycleArray()
+//     4) Insert the genere as text in the list-item//
+newLiNode.innerText = genres[i]
+
+//     5) Append the list items in the unordered list//
+newUl.appendChild(newLiNode)
+
+//     6) Append the unordered list in the document. The list should appear in the page.//
+document.appendChild(newUl)
